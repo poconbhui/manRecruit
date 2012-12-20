f917f87830e52d7c349eeead4aef21f68b2511d4
Padraig O Conbhui <poconbhui@gmail.com>
2012-12-20 03:36:44 +0000

    Further testing MongoDB connection.

--- models/nations.js
+++ models/nations.js
@@ -213,7 +213,7 @@ var Nations = function(region_in){
         return false;
       }
 
-      data._id = data.nation;
+      //data._id = data.nation;
       nation_collection.insert(data, {w:1}, function(error,result){
         console.log('INSERTED ATTEMPT: ',data);
         console.log('INSERTED ERROR: ',error);
